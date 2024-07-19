06c3c3c72737151c4bbcba29d732dfb49ce71224
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2024-07-19 23:38:07 +0530

    shopping item element on lazy column

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -29,12 +33,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import java.util.Locale
 
 
 data class ShoppingItem(val id:Int,
@@ -75,7 +83,9 @@ fun ShoppingList(){
                     .fillMaxSize()
                     .padding(16.dp)
             ){
-                items(sItems){}
+                items(sItems){
+                    ShoppingListItem(it,{},{})
+                }
             }
         }
 
@@ -104,7 +114,7 @@ fun ShoppingList(){
                                 Row(modifier = Modifier
                                     .fillMaxWidth(),
                                 horizontalArrangement = Arrangement.SpaceBetween) {
-
+                                    //add button
                                     Button(
                                         onClick = {
                                                   if (itemName.isNotBlank() && itemQuantity.isNotBlank()){
@@ -122,7 +132,7 @@ fun ShoppingList(){
                                         modifier = Modifier.size(width = 96.dp, height = 48.dp)) {
                                         Text(text = "Add")
                                     }
-
+                                    //cancel button
                                     Button(
                                         onClick = { showDialog = false},
                                         modifier = Modifier.size(width = 96.dp, height = 48.dp)) {
@@ -167,7 +177,36 @@ fun ShoppingListItem(
     onEditClick: () -> Unit,
     onDeleteClick: () -> Unit,
 ){
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = item.name.capitalize(Locale.ROOT), modifier = Modifier.padding(8.dp))
+                Text(text = "QTY :", modifier = Modifier.padding(8.dp))
+                Text(text = item.quantity.toString(), modifier = Modifier.padding(8.dp))
 
+                Spacer(modifier = Modifier.width(24.dp))
+
+                Button(
+                    shape = RoundedCornerShape(16.dp),
+                    modifier = Modifier.padding(8.dp).fillMaxHeight(),
+                    onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+                }
+
+                Button(
+                    shape = RoundedCornerShape(16.dp),
+                    modifier = Modifier.padding(8.dp).fillMaxHeight(),
+                    onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+                }
+            }
+    }
 }
 
 

102e6cb0c67da049940180c1be91485bf619b10c
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2024-07-21 19:22:44 +0530

    ShoppingIteEditor

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,9 +14,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -22,6 +26,8 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -34,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.capitalize
@@ -173,6 +180,50 @@ fun ShoppingList(){
 
 
 @Composable
+fun ShoppingListItemEditor(
+    item : ShoppingItem,
+    onEditComplete: (String,Int) -> Unit
+){
+    var editedName by remember {mutableStateOf(item.name) }
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+    Row (
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+
+    ){
+        ElevatedCard {
+            Column {
+                BasicTextField(
+                    value = editedName,
+                    onValueChange = {editedName = it},
+                    singleLine = true,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(8.dp)
+                )
+                BasicTextField(
+                    value = editedQuantity,
+                    onValueChange = {editedQuantity = it},
+                    singleLine = true,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(8.dp)
+                )
+            }
+        }
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+        }) {
+            Text(text = "Save")
+        }
+    }
+}
+
+@Composable
 fun ShoppingListItem(
     item: ShoppingItem,
     onEditClick: () -> Unit,
@@ -188,12 +239,10 @@ fun ShoppingListItem(
                 modifier = Modifier.padding(8.dp),
                 verticalAlignment = Alignment.CenterVertically) {
                 Text(text = item.name.capitalize(Locale.ROOT), modifier = Modifier.padding(8.dp))
-                Text(text = "QTY :", modifier = Modifier.padding(8.dp))
-                Text(text = item.quantity.toString(), modifier = Modifier.padding(8.dp))
+                Text(text = "QTY : ${item.quantity}", modifier = Modifier.padding(8.dp))
 
-                Spacer(modifier = Modifier.width(88.dp))
 
-                Row {
+                Row(modifier = Modifier.padding(8.dp)) {
                     //edit element
                     IconButton(onClick = { onEditClick }) {
                         Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
